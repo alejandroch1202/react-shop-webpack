c066d7d182a90d9465b899800908c6f2ba2a0435
alejandroch1202 <alejandrochavez1202@gmail.com>
2023-02-09 09:56:28 -0400

    Added assets and alias

--- src/pages/SendEmail.jsx
+++ src/pages/SendEmail.jsx
@@ -1,21 +1,24 @@
 import React from "react";
-import "../styles/SendEmail.scss";
+import "@styles/SendEmail.scss";
+
+import logo from "@logos/logo_yard_sale.svg";
+import email from "@icons/email.svg";
 
 const SendEmail = () => {
   return (
     <div className="SendEmail">
       <div className="form-container">
-        <img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
+        <img src={logo} alt="logo" className="logo" />
         <h1 className="title">Email has been sent!</h1>
         <p className="subtitle">
           Please check your inbox for instructions on how to reset the password
         </p>
         <div className="email-image">
-          <img src="./icons/email.svg" alt="email" />
+          <img src={email} alt="email" />
         </div>
         <button className="primary-button login-button">Login</button>
         <p className="resend">
-          <span>Didn't receive the email?</span>
+          <span>Didn't receive the email? </span>
           <a href="/">Resend</a>
         </p>
       </div>
