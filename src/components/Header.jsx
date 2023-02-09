1289ad7ef0ceb387fa14f8e493a71059cac1c210
alejandroch1202 <alejandrochavez1202@gmail.com>
2023-02-09 15:59:20 -0400

    Adding context, setting up My Orders menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "@styles/Header.scss";
-
+import Menu from "@components/Menu";
 import menu from "@icons/icon_menu.svg";
 import logo from "@logos/logo_yard_sale.svg";
+import MyOrder from "@containers/MyOrder";
+import AppContext from "@context/AppContext";
 import shoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+  const [toggleOrders, setToggleOrders] = useState(false);
+  const { state } = useContext(AppContext);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
+  const handleToggleOrders = () => {
+    setToggleOrders(!toggleOrders);
+  };
+
   return (
     <nav>
       <img src={menu} alt="menu" className="menu" />
@@ -34,13 +48,21 @@ const Header = () => {
       </div>
       <div className="navbar-right">
         <ul>
-          <li className="navbar-email">platzi@example.com</li>
+          <li className="navbar-email prevent-select" onClick={handleToggle}>
+            platzi@example.com
+          </li>
           <li className="navbar-shopping-cart">
-            <img src={shoppingCart} alt="shopping cart" />
-            <div>2</div>
+            <img
+              src={shoppingCart}
+              alt="shopping cart"
+              onClick={handleToggleOrders}
+            />
+            {state.cart.length ? <div>{state.cart.length}</div> : null}
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
+      {toggleOrders && <MyOrder />}
     </nav>
   );
 };
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import ProductItem from "@components/ProductItem";
 import "@styles/ProductList.scss";
+import useGetProducts from "@hooks/useGetProducts";
+
+const limit = 16;
+const API = `https://api.escuelajs.co/api/v1/products?limit=${limit}&offset=1`;
 
 const ProductList = () => {
+  const products = useGetProducts(API);
   return (
     <section className="main-container">
       <div className="ProductList">
-        <ProductItem />
+        {products.map((product) => (
+          <ProductItem product={product} key={product.id} />
+        ))}
       </div>
     </section>
   );
